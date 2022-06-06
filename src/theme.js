70760b47a94fb9c595b32c88590fff438ed542c5
Cyanhead <bunmioye09@gmail.com>
2022-06-06 15:36:49 +0100

    add global style and theme

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+export const theme = {
+  color: {
+    primary: '#3BB77E',
+    primaryLite: '#DEF9EC',
+
+    black: '#253D4E',
+    white: '#fff',
+    grey: '#adadad',
+  },
+
+  fontWght: {
+    regular: 400,
+  },
+};
