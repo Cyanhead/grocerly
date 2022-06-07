1c625cc695dd8a711fb8e2db221b978347809ec6
Cyanhead <bunmioye09@gmail.com>
2022-06-07 08:08:03 +0100

    create ChevronDown component

---
+++ src/components/ChevronDown/ChevronDown.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { IconWrap } from '../others.style';
+import { FiChevronDown } from 'react-icons/fi';
+
+const ChevronDown = () => {
+  return (
+      <IconWrap>
+        <FiChevronDown />
+      </IconWrap>
+  );
+};
+
+export default ChevronDown;
