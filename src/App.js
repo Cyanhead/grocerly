d54244cb84e489d7a62f36ee269d375448f237c9
Cyanhead <bunmioye09@gmail.com>
2022-06-09 18:31:23 +0100

    add labels component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Hero from './components/Hero';
 import Categories from './components/Categories';
+import Labels from './components/Labels';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Header />
       <Hero />
       <Categories />
+      <Labels />
       <Footer />
     </ThemeProvider>
   );
