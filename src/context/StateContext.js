917e59468e74d68fcbd0386b91b31a32275946c2
Cyanhead <bunmioye09@gmail.com>
2022-07-04 12:30:17 +0100

    add context to manage state

--- src/context/StateContext.js
+++ src/context/StateContext.js
@@ -1,51 +1,84 @@
-import React, {
-  createContext,
-  useContext,
-  useState,
-  //  useEffect
-} from 'react';
+import React, { createContext, useContext, useState } from 'react';
 import { toast } from 'react-hot-toast';
 
 const Context = createContext();
 
 export const StateContext = ({ children }) => {
-  const [
-    showCart,
-    //  setShowCart
-  ] = useState(false);
+  const [showCart, setShowCart] = useState(false);
   const [cartItems, setCartItems] = useState([]);
-  const [totalPrice, setTotalPrice] = useState();
-  const [totalQuantity, setTotalQuantity] = useState();
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [totalQuantity, setTotalQuantity] = useState(0);
   const [qty, setQty] = useState(1);
 
   const incQty = () => {
     setQty(prevQty => prevQty + 1);
   };
 
-  const onAdd = (product, quantity) => {
-    const checkProductInCart = cartItems.find(item => item.id === product.id);
+  const onAdd = (product, quantity = 1) => {
+    // * checks if product already exists in the cart and returns product
+    const checkProductInCart = cartItems.find(item => product.id === item.id);
 
     setTotalPrice(previousPrice => previousPrice + product.price * quantity);
     setTotalQuantity(previousQuantity => previousQuantity + quantity);
 
+    // * if product exists...
     if (checkProductInCart) {
-      const updatedCartItems = cartItems.forEach(cartProduct => {
-        if (cartProduct.id === product.id)
-          return {
-            ...cartProduct,
-            quantity: cartProduct.quantity + quantity,
-          };
-      });
+      const updatedCartItems = [...cartItems];
 
+      const productIndex = updatedCartItems.findIndex(
+        cartProduct => cartProduct.id === product.id
+      );
+      updatedCartItems[productIndex].quantity += quantity;
       setCartItems(updatedCartItems);
     } else {
       product.quantity = quantity;
 
       setCartItems([...cartItems, { ...product }]);
     }
+
     toast.success(
+      // todo: remove the 's' conditional
       `${qty} ${product.name}${qty === 1 ? '' : 's'} added to cart.`
     );
+  };
+
+  const onRemove = product => {
+    const newCartItems = cartItems.filter(item => item.id !== product.id);
+
+    setTotalPrice(
+      previousPrice => previousPrice - product.price * product.quantity
+    );
+    setTotalQuantity(previousQuantity => previousQuantity - product.quantity);
+    setCartItems(newCartItems);
+    toast.error(
+      // todo: remove the 's' conditional
+      `${product.quantity} ${product.name}${
+        qty === 1 ? '' : 's'
+      } removed from cart.`
+    );
+  };
+
+  const toggleCartItemQuanitity = (id, value) => {
+    const foundProduct = cartItems.find(item => item.id === id);
+    const productIndex = cartItems.findIndex(product => product.id === id);
+    const newCartItems = [...cartItems];
+
+    if (value === 'inc') {
+      newCartItems[productIndex].quantity += 1;
+
+      setCartItems(newCartItems);
+
+      setTotalPrice(prevTotalPrice => prevTotalPrice + foundProduct.price);
+      setTotalQuantity(prevTotalQuantities => prevTotalQuantities + 1);
+    } else if (value === 'dec') {
+      if (foundProduct.quantity > 1) {
+        newCartItems[productIndex].quantity -= 1;
+        setCartItems(newCartItems);
+
+        setTotalPrice(prevTotalPrice => prevTotalPrice - foundProduct.price);
+        setTotalQuantity(prevTotalQuantities => prevTotalQuantities - 1);
+      }
+    }
   };
 
   const decQty = () => {
@@ -60,13 +93,20 @@ export const StateContext = ({ children }) => {
     <Context.Provider
       value={{
         showCart,
+        setShowCart,
         cartItems,
+        setCartItems,
         totalPrice,
+        setTotalPrice,
         totalQuantity,
+        setTotalQuantity,
         qty,
+        setQty,
         incQty,
         decQty,
         onAdd,
+        onRemove,
+        toggleCartItemQuanitity,
       }}
     >
       {children}
