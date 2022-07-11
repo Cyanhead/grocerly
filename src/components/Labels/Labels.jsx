c6acdd921f13bfa57326b859524785be6d446d62
Cyanhead <bunmioye09@gmail.com>
2022-07-11 18:57:12 +0100

    make labels responsive

--- src/components/Labels/Labels.jsx
+++ src/components/Labels/Labels.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import { LabelImg, LabelContainer, LabelWrap } from './labels.style';
+import {
+  LabelImg,
+  LabelContainer,
+  LabelImgWrap,
+  LabelWrap,
+} from './labels.style';
 
 import label1 from '../../assets/images/label1.svg';
 import label2 from '../../assets/images/label2.svg';
 import label3 from '../../assets/images/label3.svg';
 
 const Labels = () => {
-  const LabelImages = () => {
-    const images = [label1, label2, label3];
+  const images = [label1, label2, label3];
 
-    return (
-      <>
-        {images.map((img, i) => {
-          return <LabelImg key={i} src={img} alt="" />;
-        })}
-      </>
-    );
-  };
   return (
     <LabelContainer>
       <LabelWrap>
-        <LabelImages />
+        {images.map((img, i) => {
+          return (
+            <LabelImgWrap>
+              <LabelImg key={i} src={img} alt="" />
+            </LabelImgWrap>
+          );
+        })}
       </LabelWrap>
     </LabelContainer>
   );
