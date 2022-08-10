432009050f5135aa794ca68d8ff93ac7217f24cc
Cyanhead <bunmioye09@gmail.com>
2022-08-10 18:46:15 +0100

    add click outside hook

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -31,6 +31,7 @@ import {
 } from 'react-instantsearch-hooks-web';
 import algolia_logo from '../../assets/images/Algolia-nebula.svg';
 import { useProductsListContext } from '../../context/ProductsListContext';
+import { useOnClickOutside } from '../../hooks/useOnClickOutside.hook';
 
 function Hit({ hit }) {
   return (
@@ -83,41 +84,12 @@ const Searchbar = props => {
 
     const watermarkRef = useRef(null);
 
-    /**
-     * Hook that alerts clicks outside of the passed ref
-     */
-    function useOutsideAlerter(ref) {
-      useEffect(() => {
-        /**
-         * Action to do if clicked on outside of element
-         */
-        function handleClickOutside(event) {
-          if (
-            ref.current &&
-            !ref.current.contains(event.target) &&
-            !watermarkRef.current.contains(event.target)
-          ) {
-            setInsideClick(false);
-          }
-        }
-        // Bind the event listener
-        document.addEventListener('mousedown', handleClickOutside);
-        return () => {
-          // Unbind the event listener on clean up
-          document.removeEventListener('mousedown', handleClickOutside);
-        };
-      }, [ref]);
-    }
+    const ClickWrap = ({ children }) => {
+      const ref = useRef(null);
+      useOnClickOutside(ref, () => setInsideClick(false), watermarkRef);
 
-    /**
-     * Component that alerts if you click outside of it
-     */
-    function OutsideAlerter(props) {
-      const wrapperRef = useRef(null);
-      useOutsideAlerter(wrapperRef);
-
-      return <div ref={wrapperRef}>{props.children}</div>;
-    }
+      return <div ref={ref}>{children}</div>;
+    };
 
     const handleInput = value => {
       setSearchValue(value);
@@ -132,9 +104,9 @@ const Searchbar = props => {
         />
         <HitContainer showResults={insideClick && searchValue.length >= 1}>
           <HitContainerTop>
-            <OutsideAlerter>
+            <ClickWrap>
               <Hits hitComponent={Hit} />
-            </OutsideAlerter>
+            </ClickWrap>
           </HitContainerTop>
           <AlgoliaWatermark ref={watermarkRef}>
             <AlgoliaLink
