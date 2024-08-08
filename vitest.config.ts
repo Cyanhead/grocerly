276e6e9fa81c0eb70c06214d6fc5949e91a12628
Bunmi Oye <bunmioye09@gmail.com>
2024-08-08 21:19:43 +0100

    SETUP: vitest

---
+++ vitest.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    css: true,
+    setupFiles: 'src/test/setup.ts',
+    exclude: ['**/node_modules/**', '**/templates/**'],
+  },
+});
