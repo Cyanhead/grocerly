8563e180576ba6bf363a285bb76f8465738288b2
Bunmi Oye <bunmioye09@gmail.com>
2024-08-21 14:48:43 +0100

    REFACTOR: logout hook

--- src/hooks/useLogOut.hook.ts
+++ src/hooks/useLogOut.hook.ts
@@ -2,16 +2,14 @@ import { signOut } from 'firebase/auth';
 import toast from 'react-hot-toast';
 import { auth } from '../context/Firebase';
 
-export const useLogout = () => {
-  const doLogout = toast.promise(signOut(auth), {
-    loading: 'Logging out...',
-    success: 'logged out successfully',
-    error: 'Error when logging out',
-  });
-
-  doLogout
-    .then(() => {
-      return;
-    })
-    .catch(err => console.log(err.message));
+export const useLogout = async () => {
+  try {
+    await toast.promise(signOut(auth), {
+      loading: 'Logging out...',
+      success: 'logged out successfully',
+      error: 'Error when logging out',
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };
